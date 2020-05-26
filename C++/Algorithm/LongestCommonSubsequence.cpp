dbc7d7cc30792e5fedc1f6bdd90f00c921fd6bd1
aayushrai <www.aayushrai@gmail.com>
2020-05-26 23:13:32 +0530

    lcs c++ implimentation

---
+++ C++/Algorithm/LongestCommonSubsequence.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+  char s1[100],s2[100];
+  int dp[100][100];
+  cout << "Enter First String: ";
+  cin.getline(s1,100);
+  cout << "Enter Second String: ";
+  cin.getline(s2,100);
+  for(int i=0;i<=strlen(s1);i++){
+      for(int j=0;j<=strlen(s2);j++){
+          if(i == 0 || j ==0)
+            dp[i][j] = 0;
+          else if(s1[i-1] == s2[j-1])
+            dp[i][j] = 1 + dp[i-1][j-1];
+          else
+            dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+      }
+     
+  }
+  int i =strlen(s1),j = strlen(s1);
+  int lcsLen = dp[strlen(s1)][strlen(s2)];
+  int index = lcsLen;
+  char res[lcsLen+1];
+  res[index] = '\0';
+  while(i>0 and j >0)
+  {
+    if(s1[i-1] == s2[j-1]){
+          res[index-1] = s1[i-1];
+          index -= 1;
+          i -= 1;
+          j -= 1;
+    }
+    else if(dp[i-1]>dp[j-1])
+       i -= 1;
+    else
+       j -= 1;
+  }
+ cout << "Length Longest Common Subsequence : "<< lcsLen<<endl;
+ cout << "Longest Common Subsequence : "<< res;
+}
