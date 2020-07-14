d6e5d86f6ea4ffa4d7056578ee2d0695b2c120b0
aayushrai <www.aayushrai@gmail.com>
2020-07-14 23:16:46 +0530

    lis

---
+++ Python/Algorithm/DP/LIS.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == "__main__":
+
+	arr =[10, 22, 9, 33, 21, 50, 41, 60] 
+	dp = [1 for i in range(len(arr))]
+	for i in range(len(arr)):
+		for j in range(0,i):
+			if arr[j]<arr[i] and dp[i]< dp[j] + 1 :
+				dp[i] += 1
+	print(dp)
+	print(max(dp))
