c78af6bbe431e09ccab619f389071fd145bee3c6
aayushrai <www.aayushrai@gmail.com>
2021-06-06 01:20:19 +0530

    graph

---
+++ C++/Data structure/graph/find_all_paths_from_src2dst.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class graph{
+    public:
+        int vertex;
+        vector<vector<int>> adjMatrix;
+        graph(int vertex){
+            this->vertex = vertex;
+            adjMatrix.resize(vertex,vector<int>());
+        }
+        
+        void addEdge(int src,int dst){
+            adjMatrix[src].push_back(dst);
+        }
+
+        void printAllPathsFromSrc2dst(int src,int dst,vector<bool>visited,vector<int> path){
+            if(src==dst){
+                path.push_back(src);
+                for(int i:path){
+                    cout << i << " ";
+                }
+                cout << endl;
+            }
+            path.push_back(src);
+            for(int j:adjMatrix[src]){
+                if(!visited[j]){
+                    visited[j] = true;
+                    printAllPathsFromSrc2dst(j,dst,visited,path);
+                    visited[j] = false;
+                }
+            }
+
+        }
+
+};
+
+int main(){
+    int vertex = 7;
+    graph G(vertex);
+    G.addEdge(0,1);
+    G.addEdge(0,3);
+    G.addEdge(1,2);
+    G.addEdge(2,3);
+    G.addEdge(3,4);
+    G.addEdge(4,5);
+    G.addEdge(4,6);
+    G.addEdge(5,6);
+    vector<bool>visited(vertex,false);
+     G.printAllPathsFromSrc2dst(0,0,visited,{});
+
+
+    return 0;
+}
