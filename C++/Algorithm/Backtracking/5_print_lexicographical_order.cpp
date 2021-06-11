e733260ef826a170d935563112d63fed0833681c
aayushrai <www.aayushrai@gmail.com>
2021-06-11 17:46:41 +0530

    bt

---
+++ C++/Algorithm/Backtracking/5_print_lexicographical_order.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void printLexicographicalOrder(int n,int output){
+    if(output>n )
+        return;
+    cout << output << endl;
+    for(int i=0;i<=9;i++){
+        if(output == 0 and i==0 ){
+            continue;
+        }
+        printLexicographicalOrder(n,10*output + i);
+    }
+}
+
+int main(){
+    int n=1000;
+    printLexicographicalOrder(n,0);
+}
---
+++ C++/Algorithm/Backtracking/6_count_number_of_island.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+void removeConnectedIsland(int i,int j,vector<vector<int>>& mp){
+    if(i<0 or j<0 or i>=mp.size() or j>=mp[0].size() or mp[i][j]!=1)
+        return;
+    mp[i][j] = 0;
+    removeConnectedIsland(i+1,j,mp);
+    removeConnectedIsland(i-1,j,mp);
+    removeConnectedIsland(i,j+1,mp);
+    removeConnectedIsland(i,j-1,mp);
+    removeConnectedIsland(i-1,j-1,mp);
+    removeConnectedIsland(i+1,j+1,mp);
+    removeConnectedIsland(i+1,j-1,mp);
+    removeConnectedIsland(i-1,j+1,mp);
+}
+
+int main(){
+   
+    vector<vector<int>> mp =  {
+        { 1, 0, 1, 0, 0, 0, 1, 1, 1, 1 },
+        { 0, 0, 1, 0, 1, 0, 1, 0, 0, 0 },
+        { 1, 1, 1, 1, 0, 0, 1, 0, 0, 0 },
+        { 1, 0, 0, 1, 0, 1, 0, 0, 0, 0 },
+        { 1, 1, 1, 1, 0, 0, 0, 1, 1, 1 },
+        { 0, 1, 0, 1, 0, 0, 1, 1, 1, 1 },
+        { 0, 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+        { 0, 0, 0, 1, 0, 0, 1, 1, 1, 0 },
+        { 1, 0, 1, 0, 1, 0, 0, 1, 0, 0 },
+        { 1, 1, 1, 1, 0, 0, 0, 1, 1, 1 }
+    };
+    //  1 is island
+    //  0 is water
+    //  count number of island
+    // virtual graph not real graph
+    int vertex = mp.size();
+    int island = 0;
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            if(mp[i][j]==1){
+                removeConnectedIsland(i,j,mp);
+                island++;
+            }
+                
+        }
+    }
+   
+    cout << island << endl;
+    return 0;
+}
